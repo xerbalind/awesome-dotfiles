df3d129f6d9a75b441898017884bf0fc5faf5515
Xander <xandbi@gmail.com>
2022-09-19 18:25:07 +0200

    FIX path error

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+# Current script directory
+CDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # Clone my fork of awesome widgets
-rm .config/awesome/awesome-wm-widgets -Rf
-git clone https://github.com/xerbalind/awesome-wm-widgets.git .config/awesome/awesome-wm-widgets
+rm $CDIR/.config/awesome/awesome-wm-widgets -Rf
+git clone https://github.com/xerbalind/awesome-wm-widgets.git $CDIR/.config/awesome/awesome-wm-widgets
 
-export STOW_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-stow . --ignore 'LICENSE|.*\.me|setup.sh' --target $HOME
+stow . --dir=$CDIR --ignore 'LICENSE|.*\.me|setup.sh' --target $HOME
 
