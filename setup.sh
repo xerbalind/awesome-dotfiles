642c7a81b46ae39b1fdfda582017ace8418f7c6e
Xander <xandbi@gmail.com>
2022-09-20 12:27:44 +0200

    Fix no .config exists yet error

--- setup.sh
+++ setup.sh
@@ -8,5 +8,6 @@ rm $CDIR/.config/awesome/awesome-wm-widgets -Rf
 git clone https://github.com/xerbalind/awesome-wm-widgets.git $CDIR/.config/awesome/awesome-wm-widgets
 
 
+mkdir $HOME/.config/ -p
 stow . --dir=$CDIR --ignore 'LICENSE|.*\.me|setup.sh' --target $HOME
 
